cf4cb0a4d8f64969806e21ed8a9090be06c6aa2f
fauzhanFARTF <fauzan1812@gmail.com>
2021-04-07 22:25:13 +0700

    13. Penambahan base_url di script js dalam file viewhead.php

--- application/views/layout/viewhead.php
+++ application/views/layout/viewhead.php
@@ -14,8 +14,8 @@
     </head>
 
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="<?=base_url()?>asset/template/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?=base_url()?>asset/template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+<script src="<?=base_url()?>asset/template/js/adminlte.min.js"></script>
