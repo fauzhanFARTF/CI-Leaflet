98ae662a1cbf01212636dfdcd4de99a703aa05ab
fauzhanFARTF <fauzan1812@gmail.com>
2021-04-07 22:23:02 +0700

    12. Konfigurasi file views/layout/viewhead.php + penambahan script plugin javascript

--- application/views/layout/viewhead.php
+++ application/views/layout/viewhead.php
@@ -1,14 +1,21 @@
+<!DOCTYPE html>
 <html lang="en">
-<head>
- <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <meta http-equiv="x-ua-compatible" content="ie=edge">
- <title>AdminLTE 3 | Top Navigation</title>
-<!-- Font Awesome Icons -->
- <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
- <!-- Theme style -->
- <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
- <!-- Google Font: Source Sans Pro -->
- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="styleshee
-t">
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>AdminLTE 3 | Top Navigation</title>
+        <!-- Font Awesome Icons -->
+        <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+        <!-- Google Font: Source Sans Pro -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    </head>
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
