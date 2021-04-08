bcb576f42fa3a25cddddcbb2cc2765702d4cccd8
fauzhanFARTF <fauzan1812@gmail.com>
2021-04-09 00:08:08 +0700

    23_Edit Layout 2

--- application/views/layout/viewheader.php
+++ application/views/layout/viewheader.php
@@ -2,11 +2,11 @@
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+  <nav class="main-header navbar navbar-expand-md navbar-light navbar-dark"> <!-- Ganti White Menjadi Darl -->
     <div class="container">
         <a href="<?=base_url()?>assets/template/index3.html" class="navbar-brand">
-            <img src="<?=base_url()?>assets/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <img src="<?=base_url()?>assets/images/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light" style="color:gold">Geo-Info3</span>
         </a>
       
       <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
