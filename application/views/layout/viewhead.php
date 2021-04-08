a52650db77323510b851fb1c268500faaf1c6f86
fauzhanFARTF <fauzan1812@gmail.com>
2021-04-08 21:52:36 +0700

    19. Memanggil file leaflet.groupedlayercontrol.css dan leaflet.groupedlayercontrol.js dan memanggil peta google satellite hybrid

--- application/views/layout/viewhead.php
+++ application/views/layout/viewhead.php
@@ -13,8 +13,9 @@
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+        <link rel="stylesheet" href="<?=base_url()?>assets/leaflet.groupedlayercontrol.css" />
     </head>
-    
+
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <!-- jQuery -->
     <script src="<?=base_url()?>assets/template/plugins/jquery/jquery.min.js"></script>
@@ -24,4 +25,5 @@
     <script src="<?=base_url()?>assets/template/js/adminlte.min.js"></script>
     <!-- AdminLTE for demo purposes -->
     <script src="<?=base_url()?>assets/template/dist/js/demo.js"></script>
+    <script src="<?=base_url()?>assets/leaflet.groupedlayercontrol.js"></script>
     
