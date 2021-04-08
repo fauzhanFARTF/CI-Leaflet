5612a178e222687d6a334b8a66ea2a9efbfe3755
fauzhanFARTF <fauzan1812@gmail.com>
2021-04-08 07:01:38 +0700

    14. Edit Konfigurasi Layout

--- application/views/layout/viewhead.php
+++ application/views/layout/viewhead.php
@@ -6,16 +6,18 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>AdminLTE 3 | Top Navigation</title>
         <!-- Font Awesome Icons -->
-        <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+        <link rel="stylesheet" href="<?=base_url()?>asset/template/plugins/fontawesome-free/css/all.min.css">
         <!-- Theme style -->
-        <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+        <link rel="stylesheet" href="<?=base_url()?>asset/template/dist/css/adminlte.min.css">
         <!-- Google Font: Source Sans Pro -->
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     </head>
 
-<!-- jQuery -->
-<script src="<?=base_url()?>asset/template/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="<?=base_url()?>asset/template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="<?=base_url()?>asset/template/js/adminlte.min.js"></script>
+    <!-- jQuery -->
+    <script src="<?=base_url()?>assets/template/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?=base_url()?>assets/template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?=base_url()?>assets/template/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?=base_url()?>assets/template/dist/js/demo.js"></script>
